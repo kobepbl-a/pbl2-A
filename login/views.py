efe871f1a6e14842b8b2d5cc8bd646fa150e9a1c
uiuiuiuiuiiiin <hiroya4011@icloud.com>
2023-06-07 01:17:17 +0000

    first commit

--- login/views.py
+++ login/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class views():
+    
+    def signup_view():
+        pass
+    
+    def signin_view():
+        pass    
+    
+    def signout_view():
+        pass
+    
+    
